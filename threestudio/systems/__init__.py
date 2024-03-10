5787335cfbf010d6d99c50d158176500918e2844
Glanceyes <glanceyes@naver.com>
2024-03-10 22:39:51 +0900

    Add first commit

---
+++ threestudio/systems/__init__.py
@@ -0,0 +1,16 @@
+from . import (
+    control4d_multiview,
+    dreamfusion,
+    dreamflow,
+    fantasia3d,
+    imagedreamfusion,
+    instructnerf2nerf,
+    latentnerf,
+    magic3d,
+    magic123,
+    prolificdreamer,
+    sjc,
+    textmesh,
+    zero123,
+    zero123_simple,
+)
